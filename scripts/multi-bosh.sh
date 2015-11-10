9203859f6d54026742d259b06da3903933fbcb85
Laurent <llaffont@afi-sa.fr>
2015-11-10 10:57:57 +0100

    multi bosh now works

--- scripts/multi-bosh.sh
+++ scripts/multi-bosh.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
-# use : bash ./multi-bosh PATH_BOSH PATH_HTDOCS PATH_SCRIPT
-# opac mutu :  bash ./multi-bosh.sh "/var/www/html/vhosts/opac2/www/php/bosh/bosh.php" "/var/www/html/vhosts/opac2/www/htdocs" "/var/www/html/vhosts/opac2/www/php/test.php"
-# wip: bash ./multi-bosh.sh "/var/www/html/vhosts/opac2/www/htdocs/stl/bosh/bosh.php" "/var/www/html/vhosts/opac2/www/htdocs/wip" test.php
+# ./multi-bosh PATH_BOSH PATH_HTDOCS PATH_SCRIPT
 run () {
     echo "$@"
     $@
 }
 
-boshpath=$1
+
 defaultpath=$2
+boshpath="$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
+scriptpath="$(cd "$(dirname "$3")" && pwd)/$(basename "$3")"
+
 
 sites=$(find $defaultpath  -maxdepth 1 -type d)
 for site in $sites; do
-    cd $site
-    if [[ -r config.ini ]]
-    then
-       $boshpath exec $3 
-    fi
+    (
+        cd $site
+        if [[ -r config.ini ]]
+        then
+            echo "$site:"
+            $boshpath exec $scriptpath
+            echo ""
+        fi
+    )
 done
-
