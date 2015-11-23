b452232122c821f55724816bc278aceebb72c172
llaffont <llaffont@afi-sa.fr>
2015-11-23 16:55:03 +0100

    faster update bokeh

--- scripts/upgrade-opac.sh
+++ scripts/upgrade-opac.sh
@@ -4,20 +4,21 @@ git pull --rebase
 source ./install.sh
 
 
-directories=`find  -P ~/www/htdocs/  -maxdepth 4 -type d -path "*/skins/*/.git" | sed "s/.git//g"`
-for dir in $directories
-do
-	echo "update skin for: $dir"
-	cd $dir;
-	git  pull --rebase;
-done
-
 directories=`find  -P ~/www/htdocs/  -maxdepth 2 -type d -path "*/cosmogramme" `
 for dir in $directories
 do
 	echo "patch: $dir"
 	cd $dir 
 	php php/upgrade_db.php
+done
+
+
+directories=`find  -P ~/www/htdocs/  -maxdepth 3 -type d -path "*/skins/*/"`
+for dir in $directories
+do
+	echo "update skin for: $dir"
+	cd $dir;
+	git  pull --rebase;
 done
 
              
