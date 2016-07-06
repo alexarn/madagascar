9ccc98051456c8aee5220d2fff3fdbad9560f9b7
Patrick Barroca <pbarroca@afi-sa.fr>
2016-07-06 18:12:34 +0200

    new hosting platform upgrade script versionned

---
+++ scripts/upgrade-opac-online.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+cd ~/www/php/
+git pull --rebase
+./install.sh
+
+### Màj des Bokeh dédiés ###
+#
+directories=$(find  -P ~/www/htdocs-dedi/  -maxdepth 2 -type d -path "*/cosmogramme")
+for dir in $directories
+do
+        echo "patch: $dir"
+        cd $dir
+        php php/upgrade_db.php
+done
+
+directories=$(find  -P ~/www/htdocs-dedi/ -maxdepth 3 -type d -path "*/skins/*")
+for dir in $directories
+do
+        echo "update skin for: $dir"
+        cd $dir;
+        git  pull --rebase;
+done
+
+### Màj des Bokeh mutus ###
+#
+directories=$(find  -P ~/www/htdocs-mutu/  -maxdepth 2 -type d -path "*/cosmogramme")
+for dir in $directories
+do
+        echo "patch: $dir"
+        cd $dir
+        php php/upgrade_db.php
+done
+
+directories=$(find  -P ~/www/htdocs-mutu/ -maxdepth 3 -type d -path "*/skins/*")
+for dir in $directories
+do
+        echo "update skin for: $dir"
+        cd $dir;
+        git  pull --rebase;
+done
+
+sudo ansible-playbook /etc/ansible/playbooks/restart_bokeh_services.yml
