6f55d73640648877c841ece5f21479738a8bee7c
Patrick Barroca <pbarroca@sandbox.pergame.net>
2016-10-05 17:45:01 +0200

    branch management playbooks factorisation

--- scripts/restore-blessed.sh
+++ scripts/restore-blessed.sh
@@ -8,4 +8,4 @@ fi
 
 CLIENT_DIR=$1
 
-ansible-playbook -vvvv scripts/ansible-playbook-restore-blessed-bokeh.yml --extra-vars "directory=$CLIENT_DIR"
+ansible-playbook -vvvv scripts/ansible-playbook-branch-management-bokeh.yml --extra-vars "{'directory':'$CLIENT_DIR','branch_command':'restore-blessed.sh'}"
