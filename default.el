8c18e2acf3db03ce2878be928eff15a614ef993d
Ghislain Loas <ghislo@sandbox.pergame.net>
2016-12-06 11:12:08 +0100

    update default.el

--- default.el
+++ default.el
@@ -6,17 +6,27 @@
 (require 'phafi-autoload)
 (defun my-phafi-mode()
   (setq userpath (concat  "~/htdocs/" user-login-name ))
-  (if (string-match "cosmogramme" (buffer-file-name))
-      (setq phafi-phpunit-config (concat userpath "/cosmogramme/tests/phpunit.xml"))
+
+  (if (string-match "cosmozend/" (buffer-file-name))
+      (setq phafi-phpunit-config (concat userpath "/cosmogramme/cosmozend/tests/phpunit.xml"))
+
     (if (string-match "multimedia/" (buffer-file-name))
 	(setq phafi-phpunit-config "~/public_html/multimedia/var/www/multimedia/tests/phpunit.xml")
 
-      (if (string-match "websvc/" (buffer-file-name))
-	  (setq phafi-phpunit-config "~/public_html/websvc/tests/phpunit.xml")
+      (if (string-match (concat user-login-name "/websvc/") (buffer-file-name))
+	  (setq phafi-phpunit-config (concat userpath "/websvc/tests/phpunit.xml"))
 
-	(if (string-match "storm/" (buffer-file-name))
-	    (setq phafi-phpunit-config  (concat userpath "/library/storm/tests/phpunit.xml"))
-	  (setq phafi-phpunit-config (concat userpath "/tests/phpunit.xml"))
+	(if (string-match "websvc/" (buffer-file-name))
+	    (setq phafi-phpunit-config "~/htdocs/websvc/tests/phpunit.xml")
+	  
+	  (if (string-match "storm/" (buffer-file-name))
+	      (setq phafi-phpunit-config  (concat userpath "/library/storm/tests/phpunit.xml"))
+
+	    (if (string-match (concat user-login-name "/cosmogramme") (buffer-file-name))
+		(setq phafi-phpunit-config (concat userpath "/cosmogramme/tests/phpunit.xml"))
+
+	      (setq phafi-phpunit-config (concat userpath "/tests/phpunit.xml")))
+	    )
 	  )
 	)
       )
@@ -25,18 +35,8 @@
 
 (add-hook 'phafi-mode-hook 'my-phafi-mode)
 
-(defun dump-db(dbname)
-  (interactive (list (read-string "DB name: ")))
-  (async-shell-command 
-   (concat 
-    "echo 'drop database if exists " dbname "; create database " dbname "'|mysql -uroot -proot;"
-    "ssh webmaster@web.afi-sa.net 'sh ~/dump_db.sh' " dbname "|mysql -uroot -proot " dbname))
-)
-
-
 (custom-set-variables
  '(menu-bar-mode nil)
- '(truncate-lines t)
  '(show-paren-mode t))
 
 (setq iswitchb-buffer-ignore '("^ "))
@@ -56,5 +56,22 @@
 (require 'smex)
 (global-set-key (kbd "M-x") 'smex)
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
-;; This is your old M-x.
 (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+(global-set-key (kbd "M-[ C") 'windmove-right)
+(global-set-key (kbd "M-[ D") 'windmove-left)
+(global-set-key (kbd "M-[ a") 'windmove-up)
+(global-set-key (kbd "M-[ B") 'windmove-down)
+(global-set-key [f8] 'next-error)
+(global-set-key [f5] 'ag)
+(iswitchb-mode 't)
+
+(custom-set-faces)
+
+(setq geben-dbgp-default-port 9001) 
+
+(defun dump_db(dbname)
+  (interactive (list (read-string "DB name: ")))
+  (async-shell-command
+   (concat "bash dump_db " dbname))
+)
+
