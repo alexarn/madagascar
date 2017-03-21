816d89c5b5687af5eeb4858debec8f688488af29
Laurent Laffont <llaffont@afi-sa.fr>
2017-03-21 10:31:20 +0100

    fix ansible scripts as JSON not supported anymore

--- scripts/install-branch.sh
+++ scripts/install-branch.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-usage='Usage: bash install-branch.sh <git_branch_name> <client_directory>'
+usage='Usage: bash install-branch.sh <git_branch_name> <client_directory>
+ex: bash scripts/install-branch.sh hotline#47207_message_ressources_numeriques_cvs htdocs-mutu/le-gresivaudan.fr
+'
 if [ $# -lt 2 ]; then
   printf "$0: not enough arguments\n%s\n" "$usage" >&2
   exit 2
@@ -9,4 +11,4 @@ fi
 BRANCH=$1
 CLIENT_DIR=$2
 
-ansible-playbook -vvvv scripts/ansible-playbook-branch-management-bokeh.yml --extra-vars "{'directory':'$CLIENT_DIR','branch_command':'install-branch.sh $BRANCH'}"
+ansible-playbook -vvvv scripts/ansible-playbook-branch-management-bokeh.yml --extra-vars "directory='$CLIENT_DIR' branch_command='install-branch.sh $BRANCH'"
--- scripts/restore-blessed.sh
+++ scripts/restore-blessed.sh
@@ -8,4 +8,4 @@ fi
 
 CLIENT_DIR=$1
 
-ansible-playbook -vvvv scripts/ansible-playbook-branch-management-bokeh.yml --extra-vars "{'directory':'$CLIENT_DIR','branch_command':'restore-blessed.sh'}"
+ansible-playbook -vvvv scripts/ansible-playbook-branch-management-bokeh.yml --extra-vars "directory='$CLIENT_DIR' branch_command='restore-blessed.sh'"
